e92cb83a066cfb32a1b27684a802c1e198e5deab
Kep <KepCreations@gmail.com>
2021-09-26 14:23:03 -0400

    Add favorite star to HTML, isFavorite test

--- js/stories.js
+++ js/stories.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // This is the global list of the stories, an instance of StoryList
 let storyList;
@@ -6,10 +6,10 @@ let storyList;
 /** Get and show stories when site first loads. */
 
 async function getAndShowStoriesOnStart() {
-  storyList = await StoryList.getStories();
-  $storiesLoadingMsg.remove();
+	storyList = await StoryList.getStories();
+	$storiesLoadingMsg.remove();
 
-  putStoriesOnPage();
+	putStoriesOnPage();
 }
 
 /**
@@ -19,12 +19,11 @@ async function getAndShowStoriesOnStart() {
  * Returns the markup for the story.
  */
 
-function generateStoryMarkup(story) {
-  // console.debug("generateStoryMarkup", story);
+function generateStoryMarkup(star, story) {
+	// console.debug("generateStoryMarkup", story);
 
-  const hostName = story.getHostName();
-  return $(`
-      <li id="${story.storyId}">
+	const hostName = story.getHostName();
+	return $(`<li id="${story.storyId}">${star} 
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
@@ -35,30 +34,49 @@ function generateStoryMarkup(story) {
     `);
 }
 
+const isFavorite = (story) => {
+	// console.log(currentUser, story);
+	for (let fav of currentUser.favorites) {
+		console.log(fav.storyId);
+		if (fav.storyId === story.storyId) {
+			return true;
+		}
+	}
+	return false;
+};
+
 /** Gets list of stories from server, generates their HTML, and puts on page. */
 
 function putStoriesOnPage() {
-  console.debug("putStoriesOnPage");
+	console.debug('putStoriesOnPage');
 
-  $allStoriesList.empty();
+	$allStoriesList.empty();
 
-  // loop through all of our stories and generate HTML for them
-  for (let story of storyList.stories) {
-    const $story = generateStoryMarkup(story);
-    $allStoriesList.append($story);
-  }
+	let star;
+	// loop through all of our stories and generate HTML for them
+	for (let story of storyList.stories) {
+		if (!currentUser) {
+			star = '';
+		} else {
+			isFavorite(story)
+				? (star = '<i class="fas fa-star" />')
+				: (star = '<i class="far fa-star" />');
+		}
+		const $story = generateStoryMarkup(star, story);
+		$allStoriesList.append($story);
+	}
 
-  $allStoriesList.show();
+	$allStoriesList.show();
 }
 
-$submitStoryForm.on("submit", submitNewStory)
+$submitStoryForm.on('submit', submitNewStory);
 
 function submitNewStory(evt) {
-    evt.preventDefault()
-    console.debug('submitNewStory', evt)
-    const author = $('#story-author').val()
-    const title = $('#story-title').val()
-    const url = $('#story-url').val()
+	evt.preventDefault();
+	console.debug('submitNewStory', evt);
+	const author = $('#story-author').val();
+	const title = $('#story-title').val();
+	const url = $('#story-url').val();
 
-    const newStory = storyList.addStory(currentUser, { title, author, url });
+	const newStory = storyList.addStory(currentUser, { title, author, url });
 }
