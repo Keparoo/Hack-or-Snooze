0d64126f16ffb5673c63aebc78d44aa00d41bc1b
Kep <KepCreations@gmail.com>
2021-09-28 14:27:38 -0400

    Refactor funcs in nav.js to arrow functions

--- js/nav.js
+++ js/nav.js
@@ -6,54 +6,54 @@
 
 /** Show main list of all stories when click site name */
 
-function navAllStories(evt) {
+const navAllStories = (evt) => {
 	console.debug('navAllStories', evt);
 	hidePageComponents();
 	putStoriesOnPage();
-}
+};
 
 $body.on('click', '#nav-all', navAllStories);
 
 /** Show login/signup on click on "login" */
 
-function navLoginClick(evt) {
+const navLoginClick = (evt) => {
 	console.debug('navLoginClick', evt);
 	hidePageComponents();
 	$loginForm.show();
 	$signupForm.show();
-}
+};
 
 $navLogin.on('click', navLoginClick);
 
 /** When a user first logins in, update the navbar to reflect that. */
 
-function updateNavOnLogin() {
+const updateNavOnLogin = () => {
 	console.debug('updateNavOnLogin');
 	$('.main-nav-links').show();
 	$navLogin.hide();
 	$navLogOut.show();
 	$navUserProfile.text(`${currentUser.username}`).show();
-}
+};
 
-function navSubmitClick(evt) {
+const navSubmitClick = (evt) => {
 	console.debug('submitStory', evt);
 	$submitStory.show();
-}
+};
 
 $body.on('click', '#nav-submit', navSubmitClick);
 
-function navFavoritesClick(evt) {
+const navFavoritesClick = (evt) => {
 	console.debug('navFavoritesClick');
 	hidePageComponents();
 	putFavoritesOnPage();
-}
+};
 
 $body.on('click', '#nav-favorites', navFavoritesClick);
 
-function myStoriesClick(evt) {
+const myStoriesClick = (evt) => {
 	console.debug('myStoriesClick');
 	hidePageComponents();
 	putmyStoriesOnPage();
-}
+};
 
 $body.on('click', '#nav-my-stories', myStoriesClick);
