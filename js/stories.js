dcfeebdc386a9fde52425d4e8f31c8aac5cc356c
Kep <KepCreations@gmail.com>
2021-09-28 14:08:58 -0400

    Clean up comments in stories.js

--- js/stories.js
+++ js/stories.js
@@ -19,9 +19,21 @@ const getAndShowStoriesOnStart = async () => {
  * Returns the markup for the story.
  */
 
-const makeStarMarkup = (story) => {
+// Returns true if the passed in story is a favorite
+const isFavorite = (story) => {
+	// console.debug('isFavorite');
+	for (let fav of currentUser.favorites) {
+		if (fav.storyId === story.storyId) {
+			return true;
+		}
+	}
+	return false;
+};
+
+// Create the HTML for the favorite/not favorite star
+const makeStarMarkup = (user, story) => {
 	let star = '';
-	if (currentUser) {
+	if (user) {
 		isFavorite(story)
 			? (star = '<i class="fas fa-star"></i>')
 			: (star = '<i class="far fa-star"></i>');
@@ -29,17 +41,20 @@ const makeStarMarkup = (story) => {
 	return star;
 };
 
+// Create the HTML for the delete button (trash can)
 const makeTrashMarkup = () => {
 	return '<i class="fas fa-trash-alt"></i>';
 };
 
+// Generate the HTML for a story listing
 const generateStoryMarkup = (story, showTrash = false) => {
 	// console.debug("generateStoryMarkup", story);
-	const star = makeStarMarkup(story);
+
 	let trash;
 	showTrash ? (trash = makeTrashMarkup()) : (trash = '');
-
+	const star = makeStarMarkup(currentUser, story);
 	const hostName = story.getHostName();
+
 	return $(`<li id="${story.storyId}">
         <span class="trash">${trash}</span>
         <span class="star">${star}</span>
@@ -53,16 +68,7 @@ const generateStoryMarkup = (story, showTrash = false) => {
     `);
 };
 
-const isFavorite = (story) => {
-	// console.debug('isFavorite');
-	for (let fav of currentUser.favorites) {
-		if (fav.storyId === story.storyId) {
-			return true;
-		}
-	}
-	return false;
-};
-
+// Return a story object that matches the passed in storyId
 const getStory = (storyId) => {
 	for (let story of storyList.stories) {
 		if ((story.storyId = storyId)) {
@@ -71,12 +77,14 @@ const getStory = (storyId) => {
 	}
 };
 
+// Toggle the Favorite Star for a story from Favorite to not Favorite or vice versa
 const toggleStar = (evt) => {
 	console.debug('toggleStar');
 	const $target = $(evt.target);
 	const storyId = $target.parent().parent().attr('id');
 
 	const story = getStory(storyId);
+
 	if ($target.closest('i').hasClass('fas')) {
 		currentUser.removeFavoriteStory(story);
 		$target.closest('i').toggleClass('fas far');
@@ -88,6 +96,7 @@ const toggleStar = (evt) => {
 
 $body.on('click', '.star', toggleStar);
 
+// Delete a story from the page and from the database
 const deleteStoryFromPage = async (evt) => {
 	console.debug('deleteStory');
 	const storyId = $(evt.target).parent().parent().attr('id');
@@ -98,8 +107,7 @@ const deleteStoryFromPage = async (evt) => {
 
 $body.on('click', '.trash', deleteStoryFromPage);
 
-/** Gets list of stories from server, generates their HTML, and puts on page. */
-
+// Gets list of stories from server, generates their HTML, and puts on page.
 const putStoriesOnPage = () => {
 	console.debug('putStoriesOnPage');
 
@@ -114,6 +122,7 @@ const putStoriesOnPage = () => {
 	$allStoriesList.show();
 };
 
+// Display a list of a user's favorited stories
 const putFavoritesOnPage = () => {
 	console.debug('putFavoritesOnPage');
 
@@ -131,6 +140,7 @@ const putFavoritesOnPage = () => {
 	$favoriteStoriesList.show();
 };
 
+// Display a list of a user's added stories
 const putmyStoriesOnPage = () => {
 	console.debug('putmyStoriesOnPage');
 
@@ -149,6 +159,7 @@ const putmyStoriesOnPage = () => {
 	$myStoriesList.show();
 };
 
+// Create a new story and add it to the page and database
 const submitNewStory = async (evt) => {
 	evt.preventDefault();
 	console.debug('submitNewStory', evt);
