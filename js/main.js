17915971d5e4f6f3be13899adbd367d35294327c
Kep <KepCreations@gmail.com>
2021-09-28 14:29:29 -0400

    Refactor funcs in main.js to arrow functions

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ const $navLogOut = $('#nav-logout');
  * calling this, individual components can re-show just what they want.
  */
 
-function hidePageComponents() {
+const hidePageComponents = () => {
 	const components = [
 		$allStoriesList,
 		$loginForm,
@@ -34,11 +34,11 @@ function hidePageComponents() {
 		$myStoriesList
 	];
 	components.forEach((c) => c.hide());
-}
+};
 
 /** Overall function to kick off the app. */
 
-async function start() {
+const start = async () => {
 	console.debug('start');
 
 	// "Remember logged-in user" and log in, if credentials in localStorage
@@ -47,14 +47,8 @@ async function start() {
 
 	// if we got a logged-in user
 	if (currentUser) updateUIOnUserLogin();
-}
+};
 
 // Once the DOM is entirely loaded, begin the app
 
-console.warn(
-	'HEY STUDENT: This program sends many debug messages to' +
-		" the console. If you don't see the message 'start' below this, you're not" +
-		' seeing those helpful debug messages. In your browser console, click on' +
-		" menu 'Default Levels' and add Verbose"
-);
 $(start);
